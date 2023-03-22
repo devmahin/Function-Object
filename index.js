82b590a70a50f76deaaef200cbb974b1f0d70c2e
devmahin <mahinhowlader123@gmail.com>
2023-03-23 00:25:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+
+// function add (a, b, c, d, e, f, g){
+//     var mahin = (a+ b+ c+ d+ e+ f+ g) / 75;
+//     console.log(mahin)
+// }
+// add(432,33, 443, 54, 54, 54, 55,)
+
+
+
+// function add (a, b, c, d, e, f, g, h, i, j){
+//     var hasan = (a+ b+ c+ d+ e+ f+ g+ h+ i+ j)
+//     return hasan;
+// }
+// var mot = add(11,22, 33, 44, 55, 66, 77,88, 99, 111) * 5;
+// console.log(mot)
+
+
+// var mahin = [ 543,43, 43, 4534, 34, 53, 54, 543]
+// var friendsName = ["mahin", "hasan" , "antor", "adib" , "johir" , "rezaul"]
+// function add (a){
+//     for(i=0; i <a.length; i++)
+//     console.log(a[i])
+// }
+// add(mahin)
+// add(friendsName)
+
+
+// object
+// let mahin = {
+// name : "mahin Howlader",
+// class: 'honours',
+// roll: 610,
+// age : 18
+// }
+// const antor = mahin.name;
+// const antor = mahin["name"];
+//  const antor = 'name';
+//  const adib = mahin[antor]
+// var adib = Object.keys(mahin)
+// var antor = Object.values(mahin)
+// console.log(antor)
